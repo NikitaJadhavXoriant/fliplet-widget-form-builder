aba59a0cf4206946f20695ab59b696cb86bfb383
NJadhavXoriant <Nikita.Jadhav@Xoriant.Com>
2023-01-20 18:07:10 +0530

    code changes for save button

--- js/libs/core.js
+++ js/libs/core.js
@@ -475,7 +475,7 @@ Fliplet.FormBuilder = (function() {
         switch (this._componentName) {
           case 'flDate':
           case 'flTime':
-            if (!this.value) {
+            if (this.autofill === 'custom' && !this.value) {
               _.assignIn(this.errors, {
                 requiredField: 'This field is required*'
               });
