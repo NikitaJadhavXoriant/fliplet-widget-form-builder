fe31a7169d01932bb6896a155bf19ac432f1de92
NJadhavXoriant <Nikita.Jadhav@Xoriant.Com>
2023-07-03 17:51:02 +0530

    code changes for edit mode

--- js/libs/core.js
+++ js/libs/core.js
@@ -251,7 +251,11 @@ Fliplet.FormBuilder = (function() {
       if (!component.mounted) {
         component.mounted = function() {
           if (this.defaultValueSource !== 'default') {
-            this.setValueFromDefaultSettings({ source: this.defaultValueSource, key: this.defaultValueKey });
+            var $vm = this;
+
+            setTimeout(function() {
+              $vm.setValueFromDefaultSettings({ source: this.defaultValueSource, key: this.defaultValueKey });
+            }, 0);
           }
         };
       }
