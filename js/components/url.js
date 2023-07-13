67598e82f04bcf06969cc8bd750fee9eb440ab83
Tony Wu <tonytlwu@users.noreply.github.com>
2023-07-13 18:13:32 +0100

    Consider the URL as a valid string format if there's no value provided (#537)

--- js/components/url.js
+++ js/components/url.js
@@ -24,7 +24,7 @@ Fliplet.FormBuilder.field('url', {
           // Normalize()ing to NFD Unicode normal form decomposes combined graphemes into the combination of simple ones using a regex character class to match the U+0300 → U+036F range
           value = value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
-          return value ? this.rules.urlCase.test(value) : false;
+          return value ? this.rules.urlCase.test(value) : true;
         }
       }
     };
